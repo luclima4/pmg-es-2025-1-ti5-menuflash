c3ff9742447bb44cb9112c60d046446a3fe21b70
larissamanna <larissanmanna@gmail.com>
2025-04-30 16:25:13 -0300

    JSON e pagina de menu
    criação do primeiro json com informações básicas que vão aparecer na tela do menu/cardápio e cards dinâmicos, além de uma NavBar genérica

---
+++ src/public/assets/js/dados.js
@@ -0,0 +1,76 @@
+//JSON
+
+let dados = [
+    {
+        "id": 1,
+        "titulo": "Água",
+        "valor": "R$5,00",
+        "imagem": "src/public/assets/imgs/agua.png"
+    },
+    {
+        "id": 2,
+        "titulo": "Guaraná lata",
+        "valor": "R$5,00",
+        "imagem": "src/public/assets/imgs/guaranaLata.png"
+    },
+    {
+        "id": 3,
+        "titulo": "Açaí",
+        "valor": "R$5,00",
+        "imagem": "src/public/assets/imgs/acai.png"
+    },
+    {
+        "id": 4,
+        "titulo": "Coxinha",
+        "valor": "R$5,00",
+        "imagem": "src/public/assets/imgs/coxinha.png" //mudar imagem
+    },
+    {
+        "id": 5,
+        "titulo": "Pão de queijo",
+        "valor": "R$5,00",
+        "imagem": "src/public/assets/imgs/paoDeQueijo.png" //mudar imagem
+    },
+    {
+        "id": 6,
+        "titulo": "Empada",
+        "valor": "R$5,00",
+        "imagem": "src/public/assets/imgs/empada.png" //mudar imagem
+    },
+    {
+        "id": 7,
+        "titulo": "Coca-Cola lata",
+        "valor": "R$5,00",
+        "imagem": "src/public/assets/imgs/cocaLata.png"
+    },
+    {
+        "id": 8,
+        "titulo": "Sanduíche",
+        "valor": "R$5,00",
+        "imagem": "src/public/assets/imgs/sanduiche.png" //imagem pequena
+    },
+    {
+        "id": 9,
+        "titulo": "Trident",
+        "valor": "R$5,00",
+        "imagem": "src/public/assets/imgs/trident.png"
+    },
+    {
+        "id": 10,
+        "titulo": "Trento",
+        "valor": "R$5,00",
+        "imagem": "src/public/assets/imgs/trento.png"
+    },
+    {
+        "id": 11,
+        "titulo": "Suco lata",
+        "valor": "R$5,00",
+        "imagem": "src/public/assets/imgs/sucoLata.png"
+    },
+    {
+        "id": 12,
+        "titulo": "Coca-Cola 200ml",
+        "valor": "R$5,00",
+        "imagem": "src/public/assets/imgs/coca.png"
+    }
+]
