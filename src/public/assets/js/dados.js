6bf12bf1f2be2b30d764e04a9e5d0c2aab626e32
luclima4 <lucascesarlimaral@gmail.com>
2025-05-03 13:56:01 -0300

    Adicionando dados ao JSON e Detalhes.html

--- src/public/assets/js/dados.js
+++ src/public/assets/js/dados.js
@@ -5,72 +5,134 @@ let dados = [
         "id": 1,
         "titulo": "Água",
         "valor": "R$5,00",
-        "imagem": "src/public/assets/imgs/agua.png"
+        "imagem": "src/public/assets/imgs/agua.png",
+        "disponivel": true,
+        "favorito": false,
+        "quantidade": 13,
+        
+        "semLactose": false,
+        "semGluten": true
+
     },
     {
         "id": 2,
         "titulo": "Guaraná lata",
         "valor": "R$5,00",
-        "imagem": "src/public/assets/imgs/guaranaLata.png"
+        "imagem": "src/public/assets/imgs/guaranaLata.png",
+        "disponivel": true,
+        "favorito": false,
+        "quantidade": 13,
+        "semLactose": false,
+        "semGluten": true
     },
     {
         "id": 3,
         "titulo": "Açaí",
         "valor": "R$5,00",
-        "imagem": "src/public/assets/imgs/acai.png"
+        "imagem": "src/public/assets/imgs/acai.png",
+        "disponivel": true,
+        "favorito": false,
+        "quantidade": 13,
+        "semLactose": false,
+        "semGluten": true
     },
     {
         "id": 4,
         "titulo": "Coxinha",
         "valor": "R$5,00",
-        "imagem": "src/public/assets/imgs/coxinha.png" //mudar imagem
+        "imagem": "src/public/assets/imgs/coxinha.png", //mudar imagem
+        "disponivel": true,
+        "favorito": false,
+        "quantidade": 13,
+        "semLactose": false,
+        "semGluten": true
     },
     {
         "id": 5,
         "titulo": "Pão de queijo",
         "valor": "R$5,00",
-        "imagem": "src/public/assets/imgs/paoDeQueijo.png" //mudar imagem
+        "imagem": "src/public/assets/imgs/paoDeQueijo.png", //mudar imagem
+        "disponivel": true,
+        "favorito": false,
+        "quantidade": 13,
+        "semLactose": false,
+        "semGluten": true
     },
     {
         "id": 6,
         "titulo": "Empada",
         "valor": "R$5,00",
-        "imagem": "src/public/assets/imgs/empada.png" //mudar imagem
+        "imagem": "src/public/assets/imgs/empada.png", //mudar imagem
+        "disponivel": true,
+        "favorito": false,
+        "quantidade": 13,
+        "semLactose": false,
+        "semGluten": true
     },
     {
         "id": 7,
         "titulo": "Coca-Cola lata",
         "valor": "R$5,00",
-        "imagem": "src/public/assets/imgs/cocaLata.png"
+        "imagem": "src/public/assets/imgs/cocaLata.png",
+        "disponivel": true,
+        "favorito": false,
+        "quantidade": 13,
+        "semLactose": false,
+        "semGluten": true
     },
     {
         "id": 8,
         "titulo": "Sanduíche",
         "valor": "R$5,00",
-        "imagem": "src/public/assets/imgs/sanduiche.png" //imagem pequena
+        "imagem": "src/public/assets/imgs/sanduiche.png", //imagem pequena
+        "disponivel": true,
+        "favorito": false,
+        "quantidade": 13,
+        "semLactose": false,
+        "semGluten": true
     },
     {
         "id": 9,
         "titulo": "Trident",
         "valor": "R$5,00",
-        "imagem": "src/public/assets/imgs/trident.png"
+        "imagem": "src/public/assets/imgs/trident.png",
+        "disponivel": true,
+        "favorito": false,
+        "quantidade": 13,
+        "semLactose": false,
+        "semGluten": true
     },
     {
         "id": 10,
         "titulo": "Trento",
         "valor": "R$5,00",
-        "imagem": "src/public/assets/imgs/trento.png"
+        "imagem": "src/public/assets/imgs/trento.png",
+        "disponivel": true,
+        "favorito": false,
+        "quantidade": 13,
+        "semLactose": false,
+        "semGluten": true
     },
     {
         "id": 11,
         "titulo": "Suco lata",
         "valor": "R$5,00",
-        "imagem": "src/public/assets/imgs/sucoLata.png"
+        "imagem": "src/public/assets/imgs/sucoLata.png",
+        "disponivel": true,
+        "favorito": false,
+        "quantidade": 13,
+        "semLactose": false,
+        "semGluten": true
     },
     {
         "id": 12,
         "titulo": "Coca-Cola 200ml",
         "valor": "R$5,00",
-        "imagem": "src/public/assets/imgs/coca.png"
+        "imagem": "src/public/assets/imgs/coca.png",
+        "disponivel": true,
+        "favorito": false,
+        "quantidade": 13,
+        "semLactose": false,
+        "semGluten": true
     }
 ]
